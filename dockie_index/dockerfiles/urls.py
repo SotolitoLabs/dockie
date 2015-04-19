4663f384869bb145118c70f5c3a9f27e67cf7879
Ivan Chavero <ichavero@redhat.com>
2015-04-19 17:50:38 -0600

    Add edit functionality

--- dockie_index/dockerfiles/urls.py
+++ dockie_index/dockerfiles/urls.py
@@ -6,10 +6,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'dockie_index.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', views.index),
-    url(r'^(\d+)/$', views.showDockerFile, name="show"),
+    url(r'^$', views.index, name='home'),
+    url(r'^(\d+)/$', views.showDockerFile, name='show'),
     url(r'^(\d+)$', views.showDockerFile),
-    url(r'^new$', views.newDockerFile),
+    url(r'^new$', views.newDockerFile, name='new'),
     url(r'^create$', views.createDockerFile, name='create'),
+    url(r'^edit/(\d+)$', views.editDockerFile, name='edit'),
+    url(r'^update/(\d+)$', views.updateDockerFile, name='update'),
 )
 
